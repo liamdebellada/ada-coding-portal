512fd68db3d8c7dc7a143f6675e794f7e3ca8aa6
liamdebellada <liam.debell@ada.ac.uk>
2021-02-14 15:31:23 +0000

    Added circle orbit animation to ada rings

--- components/orbit-logo.js
+++ components/orbit-logo.js
@@ -2,9 +2,12 @@ import styles from '../styles/orbit.module.css'
 export default function orbitLogo() {
     return (
         <div className={styles.outerRing}>
-            <div className={styles.innerRing}>
-                <img className={styles.adaLogo} src="/adaText.svg"/>
-            </div>
+            <div className={styles.innerRing}/>
+            <img src="/adaText.svg" className={styles.adaLogo}/>
+            <div className={`${styles.dot} ${styles['dot-2']} ${styles['dot-outer']}`}></div>
+            <div className={`${styles.dot} ${styles['dot-3']} ${styles['dot-outer']}`}></div>
+            <div className={`${styles.dot} ${styles['dot-1']}`}></div>
+            <div className={`${styles.dot} ${styles['dot-4']}`}></div>
         </div>
     )
 }
