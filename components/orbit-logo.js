7eb5855a8fcca31b18b6d869442a00f6ba149c4a
liamdebellada <liam.debell@ada.ac.uk>
2021-02-20 04:01:55 +0000

    Created new navbar layout, created new submissions view & preview components with inline code viewer

--- components/orbit-logo.js
+++ components/orbit-logo.js
@@ -1,5 +1,5 @@
 import styles from '../styles/orbit.module.css'
-export default function orbitLogo() {
+export default function orbitLogo(props) {
     return (
         <div className={styles.outerRing}>
             <div className={styles.innerRing}/>
