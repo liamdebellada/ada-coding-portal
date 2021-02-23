94f6a8b28c1617797127ed221b05ae42e67e5867
liamdebellada <liam.debell@ada.ac.uk>
2021-02-23 22:25:01 +0000

    Fixed layout bug with landing page layout at higher & lower resolutions

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export async function getServerSideProps(context) {
   }
   return {
     props: {
-      title : "Ada Nucleas",
+      title : "Ada Nucleus",
       session: s,
       challenges: staticContent
     }
